e4281062ec57925d3da6887eb94d56c66ac24379
Snowyz3 <bryanbongzihao@gmail.com>
2021-08-15 22:25:23 +0800

    15/8/2021

--- get_next_line_utils_bonus.c
+++ get_next_line_utils_bonus.c
@@ -1,29 +1,29 @@
 #include "get_next_line_bonus.h"
 
-size_t    ft_check(const char *s)
+size_t	ft_check(const char *s)
 {
-    size_t    i;
+	size_t	i;
 
 	if (!s)
 		return (0);
-    i = 0;
-    while (s[i])
-    {
-        if (s[i] == '\n')
-            return (i + 1);
-        i++;
-    }
-    return (0);
+	i = 0;
+	while (s[i])
+	{
+		if (s[i] == '\n')
+			return (i + 1);
+		i++;
+	}
+	return (0);
 }
 
-size_t    ft_strlen(const char *s)
+size_t	ft_strlen(const char *s)
 {
-    size_t    len;
+	size_t	len;
 
-    len = 0;
-    while (s[len])
-        len++;
-    return (len);
+	len = 0;
+	while (s[len])
+		len++;
+	return (len);
 }
 
 char	*ft_strljoin(char const *s1, char const *s2, size_t size)
@@ -55,56 +55,43 @@ char	*ft_strljoin(char const *s1, char const *s2, size_t size)
 	return (str);
 }
 
-int    ft_atoi(const char *nptr)
+int	ft_atoi(const char *nptr)
 {
-    int        result;
-    int        neg;
-    size_t    sign;
-    size_t    i;
+	int		result;
+	size_t	i;
 
-    result = 0;
-    sign = 0;
-    neg = 1;
-    i = 0;
-    while (nptr[i] == ' ' || (nptr[i] >= '\t' && nptr[i] <= '\r'))
-        i++;
-    while (nptr[i] == '-' || nptr[i] == '+')
-    {
-        if (nptr[i++] == '-')
-            neg = -1;
-        if (++sign > 1)
-            return (0);
-    }
-    while (nptr[i] >= '0' && nptr[i] <= '9')
-    {
-        result *= 10;
-        result += (nptr[i++] - 48);
-    }
-    return (result * neg);
+	result = 0;
+	i = 0;
+	while (nptr[i] >= '0' && nptr[i] <= '9')
+	{
+		result *= 10;
+		result += (nptr[i++] - 48);
+	}
+	return (result);
 }
 
-char    *ft_itoa(int n)
+char	*ft_itoa(int n)
 {
-    int save;
-    size_t    len;
-    char    *str;
+	size_t	len;
+	char	*str;
+	int		save;
 
-    save = n;
-    len = 0;
-    while (save > 0)
-    {
-        save /= 10;
-        len++;
-    }
+	save = n;
+	len = 0;
+	while (save > 0)
+	{
+		save /= 10;
+		len++;
+	}
 	str = malloc(sizeof(*str) * (len + 1));
-    if (!str)
-        return (0);
-    str[len--] = '\0';
-    while (n > 0)
-    {
-        str[len] = (n % 10) + 48;
-        n /= 10;
-        len--;
-    }
-    return (str);
+	if (!str)
+		return (0);
+	str[len--] = '\0';
+	while (n > 0)
+	{
+		str[len] = (n % 10) + 48;
+		n /= 10;
+		len--;
+	}
+	return (str);
 }
