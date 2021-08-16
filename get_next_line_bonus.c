99870db63f80d2e809ef724675f1b5147f794c35
Snowyz3 <bryanbongzihao@gmail.com>
2021-08-16 17:51:50 +0800

    15/8/2021

--- get_next_line_bonus.c
+++ get_next_line_bonus.c
@@ -152,7 +152,10 @@ char	*get_next_line(int fd)
 		}
 	}
 	tempbuffer[readlen] = '\0';
-	if (!buffer[fd])
+	if (buffer[fd])
+		tempint = 0;
+		//str = ft_strljoin(buffer[fd], "", 0);
+	else
 		str = ft_strljoin("\0", "", 0);
 	return (readloop(fd, buffer, str, tempbuffer));
 }
